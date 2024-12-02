f1da1db37c6e5dcb05f54aefc51d563e1d35716e
Avish-Dilaksha <adilaksha@gmail.com>
2024-12-03 00:46:23 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using GrpcToDoCRUDService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GrpcToDoCRUDService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems => Set<ToDoItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Add some dummy data here
+            modelBuilder.Entity<ToDoItem>().HasData(
+                new ToDoItem
+                {
+                    Id = 1,
+                    Title = "Buy groceries",
+                    Description = "Milk, eggs, bread, and cheese",
+                    ToDoStatus = "New"
+                },
+                new ToDoItem
+                {
+                    Id = 2,
+                    Title = "Complete homework",
+                    Description = "Finish math and science homework",
+                    ToDoStatus = "InProgress"
+                },
+                new ToDoItem
+                {
+                    Id = 3,
+                    Title = "Clean the house",
+                    Description = "Vacuum, mop, and organize the living room",
+                    ToDoStatus = "Completed"
+                }
+            );
+        }
+    }
+}
---
+++ Migrations/20241202180455_seeddata.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GrpcToDoCRUDService.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ToDoItems",
+                columns: new[] { "Id", "Description", "Title", "ToDoStatus" },
+                values: new object[,]
+                {
+                    { 1, "Milk, eggs, bread, and cheese", "Buy groceries", "New" },
+                    { 2, "Finish math and science homework", "Complete homework", "InProgress" },
+                    { 3, "Vacuum, mop, and organize the living room", "Clean the house", "Completed" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ToDoItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ToDoItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ToDoItems",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
